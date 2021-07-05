75cd64247b9d607c55e371d16dff46c20d8e25fc
GUANYI608 <74897537+GUANYI608@users.noreply.github.com>
2021-07-05 21:55:49 +0800

    feat: create not-found and sign-in page

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import SignIn from '../views/SignIn.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/signin',
+    name: 'sign-in',
+    component: SignIn
+  },
+  {
+    path: '*',
+    name: 'not-found',
+    component: () => import('../views/NotFound.vue')
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/SignIn.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <img src="../assets/AClogo.jpg" alt="LOGO" />
+
+    <h6>登入 Alphitter</h6>
+
+    <form>
+      <div class="form-group">
+        <label for="InputEmail">帳號</label>
+        <input
+          type="email"
+          class="form-control"
+          id="InputEmail"
+          aria-describedby="emailHelp"
+          placeholder="Enter email"
+        />
+      </div>
+      <div>
+        <label for="InputPassword">密碼</label>
+        <input
+          type="password"
+          class="form-control"
+          id="InputPassword"
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">登入</button>
+    </form>
+
+    <p>
+      <router-link to="/1">註冊 Alphitter</router-link>
+      ・
+      <router-link to="/2">後台登入</router-link>
+    </p>
+  </div>
+</template>
