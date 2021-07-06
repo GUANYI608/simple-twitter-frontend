dc083dd29b72c5f5258e970cb5c956c61197cfc9
GUANYI608 <74897537+GUANYI608@users.noreply.github.com>
2021-07-06 20:40:43 +0800

    style: set css file in assets and add css in signin page

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,36 +1,99 @@
 <template>
   <div class="container">
-    <img src="../assets/AClogo.jpg" alt="LOGO" />
+    <img class="logo-img" src="../assets/AClogo.jpg" alt="LOGO" />
+    <h6 class="form-title">登入 Alphitter</h6>
 
-    <h6>登入 Alphitter</h6>
-
-    <form>
+    <form class="signin-form">
       <div class="form-group">
-        <label for="InputEmail">帳號</label>
+        <label class="form-input" for="InputEmail">帳號</label>
         <input
           type="email"
           class="form-control"
           id="InputEmail"
           aria-describedby="emailHelp"
-          placeholder="Enter email"
         />
       </div>
-      <div>
-        <label for="InputPassword">密碼</label>
-        <input
-          type="password"
-          class="form-control"
-          id="InputPassword"
-          placeholder="Password"
-        />
+      <div class="form-group">
+        <label class="form-input" for="InputPassword">密碼</label>
+        <input type="password" class="form-control" id="InputPassword" />
       </div>
-      <button type="submit" class="btn btn-primary">登入</button>
+      <button type="submit" class="form-submit">登入</button>
     </form>
 
-    <p>
-      <router-link to="/1">註冊 Alphitter</router-link>
+    <p class="route-link">
+      <router-link to="/1" class="route-signup">註冊 Alphitter</router-link>
       ・
-      <router-link to="/2">後台登入</router-link>
+      <router-link to="/2" class="route-admin">後台登入</router-link>
     </p>
   </div>
 </template>
+
+
+<style scoped>
+.container {
+  width: 540px;
+  margin: 60px auto 0 auto;
+  text-align: center;
+}
+
+.logo-img {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+}
+
+.form-title {
+  margin-top: 30px;
+  font-weight: bold;
+  font-size: 23px;
+  line-height: 33px;
+}
+
+.form-group {
+  margin-top: 20px;
+  height: 50px;
+  position: relative;
+  background: #657786;
+  border-radius: 50% 50% 4px 4px;
+}
+
+.form-input {
+  position: absolute;
+  top: 5px;
+  left: 10px;
+  color: #657786;
+  font-weight: 500;
+}
+
+.form-control {
+  border: none;
+  height: 48px;
+  background: #f5f8fa;
+  border-radius: 4px;
+}
+
+.form-submit {
+  margin-top: 30px;
+  height: 50px;
+  width: 100%;
+  border-radius: 50px;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
+}
+
+.route-link {
+  margin-top: 41px;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 26px;
+  text-align: right;
+  color: #0099ff;
+}
+
+.route-signup,
+.route-admin {
+  text-decoration: underline;
+  color: #0099ff;
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <img class="logo-img" src="../assets/AClogo.jpg" alt="LOGO" />
+    <h6 class="form-title">建立你的帳號</h6>
+
+    <form class="signin-form">
+      <div class="form-group">
+        <label for="InputEmail">帳號</label>
+        <input
+          type="email"
+          class="form-control"
+          id="InputEmail"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div>
+        <label for="InputPassword">密碼</label>
+        <input type="password" class="form-control" id="InputPassword" />
+      </div>
+      <button type="submit" class="form-submit">登入</button>
+    </form>
+
+    <p>
+      <router-link to="/1">註冊 Alphitter</router-link>
+      ・
+      <router-link to="/2">後台登入</router-link>
+    </p>
+  </div>
+</template>
