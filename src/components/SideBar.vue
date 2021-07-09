49e635a2d344329757b44a0e4610e284e2b37f42
GUANYI608 <74897537+GUANYI608@users.noreply.github.com>
2021-07-09 20:30:04 +0800

    style: add tweetmodal in sidebar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,7 +2,7 @@
   <div class="sidebar">
     <img class="logo-img" src="../assets/AClogo.jpg" alt="LOGO" />
 
-    <div class="nav-list">
+    <section class="nav-list">
       <div class="nav-item">
         <img class="nav-img" src="../assets/index.jpg" alt="index" />
         <router-link class="nav-name" to="/main"> 首頁 </router-link>
@@ -15,12 +15,12 @@
         <img class="nav-img" src="../assets/setting.jpg" alt="setting" />
         <router-link class="nav-name" to="/accountsetting"> 設定 </router-link>
       </div>
-
       <button
         type="button"
         class="new-tweet"
         data-toggle="modal"
         data-target="#exampleModal"
+        @click.stop.prevent="showModal"
       >
         推文
       </button>
@@ -28,9 +28,58 @@
         <img class="nav-img" src="../assets/signout.jpg" alt="signout" />
         <span class="nav-name">登出</span>
       </div>
-    </div>
+
+      <!-- --- 彈出視窗：新增推文 --- -->
+      <!-- 彈出視窗：模糊主頁 -->
+      <div class="background-wrapper" v-if="isShowTweetModal"></div>
+      <!-- 彈出視窗：推文內容 -->
+      <div class="tweet-modal" v-if="isShowTweetModal">
+        <div class="modal-title">
+          <img
+            class="cancel-icon"
+            src="../assets/cancel.jpg"
+            alt="cancel"
+            @click.stop.prevent="cancelModal"
+          />
+        </div>
+        <div class="modal-text">
+          <img class="user-avater" src="../assets/avater.jpg" alt="avater" />
+          <div class="form-group">
+            <label class="form-input" for="NewTweet"></label>
+            <textarea
+              id="NewTweet"
+              class="tweet-content"
+              name="NewTweet"
+              rows="4"
+              cols="50"
+              placeholder="有什麼新鮮事？"
+            ></textarea>
+            <button type="submit" class="post-button">推文</button>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShowTweetModal: false,
+    };
+  },
+  methods: {
+    showModal() {
+      this.isShowTweetModal = true;
+    },
+    cancelModal() {
+      this.isShowTweetModal = false;
+    },
+  },
+};
+</script>
+
 
 <style scoped>
 .sidebar {
@@ -81,5 +130,87 @@
 .signout {
   position: fixed;
   bottom: 0;
+}
+
+/* ------ 彈出視窗 ------ */
+
+.background-wrapper {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100vw;
+  height: 100vh;
+  margin: unset;
+  padding: unset;
+  background: #363434;
+  opacity: 0.5;
+  z-index: 1;
+}
+
+.tweet-modal {
+  position: fixed;
+  top: 54px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 2;
+  width: 600px;
+  background: #ffffff;
+  border-radius: 14px;
+}
+
+.modal-title {
+  height: 55px;
+  border-bottom: 1px solid #e6ecf0;
+  display: flex;
+  align-items: center;
+}
+
+.cancel-icon {
+  height: 24px;
+  width: 24px;
+  margin-left: 15px;
+}
+
+.modal-text {
+  position: relative;
+  height: 245px;
+}
+
+.user-avater {
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  width: 50px;
+  height: 50px;
+}
+
+.tweet-content {
+  padding: 27px 15px 70px 0;
+  margin-left: 75px;
+  width: 510px;
+  font-weight: 500;
+  font-size: 18px;
+  color: #9197a3;
+  /* 去除邊框與右下角與輸入時的提示框線 */
+  border: none;
+  resize: none;
+  outline: none;
+}
+
+.tweet-content::placeholder {
+  font-weight: 500;
+  font-size: 18px;
+  color: #9197a3;
+}
+
+.post-button {
+  position: absolute;
+  right: 15px;
+  bottom: 15px;
+  height: 40px;
+  width: 64px;
+  border-radius: 100px;
+  font-weight: 500;
+  font-size: 18px;
 }
 </style>
